5a49419c50efa699b357c6f18f70b1e7fb928d0b
Pxdro-410 <pedrojuliocaso@gmail.com>
2026-05-18 03:24:37 -0600

    feat: se le agrega forma a la serpiente

--- src/components/Snake.jsx
+++ src/components/Snake.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 
-function Serpiente() {
+function Serpiente({ snake, gridSize }) {
+  if (!snake) return null;
   return (
-    <div>
-      <p style={{ color: 'mediumseagreen' }}>componente serpiente</p>
-    </div>
+    <>
+      {snake.map((segment, index) => {
+        const style = {
+          position: 'absolute',
+          left: `${(segment.x / gridSize) * 100}%`,
+          top: `${(segment.y / gridSize) * 100}%`,
+          width: `${100 / gridSize}%`,
+          height: `${100 / gridSize}%`,
+          backgroundColor: index === 0 ? '#e8e8e4ff' : 'var(--neon-green)',
+          boxShadow: index === 0 ? '0 0 15px blue' : '0 0 75px green',
+          borderRadius: index === 0 ? '6px' : '3px',
+          transition: 'left 0.1s linear, top 0.1s linear'
+        };
+
+        return (
+          <div
+            key={`${segment.x}-${segment.y}-${index}`}
+            style={style}
+          />
+        );
+      })}
+    </>
   );
 }
 
