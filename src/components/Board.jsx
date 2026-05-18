e979bd9b0d0558660c9f2d59bf6c2051842652ad
Pxdro-410 <pedrojuliocaso@gmail.com>
2026-05-18 04:35:06 -0600

    feat: pasar prop de speed desde board a snake para sincronizar animacion

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Snake from './Snake';
 import Food from './Food';
 
-function Tablero({ gridSize, snake, food }) {
+function Tablero({ gridSize, snake, food, speed }) {
   return (
     <div className="board-container" style={{ position: 'relative', width: '700px', height: '500px', overflow: 'hidden' }}>
-      <Snake snake={snake} gridSize={gridSize} />
+      <Snake snake={snake} gridSize={gridSize} speed={speed} />
       <Food food={food} gridSize={gridSize} />
     </div>
   );
